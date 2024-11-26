3a8b59a10f46eb78514b002e2d63f5eaa5c15e6a
ArtRiv <arthurfelaco707@gmail.com>
2024-11-26 17:35:31 -0300

    feat: disable sidebar default opening, add car provider

--- components/main_layout/index.tsx
+++ components/main_layout/index.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { CartProvider } from "react-use-cart"
 import { Footer } from "../footer"
 import { Header } from "../header"
 import { SidebarComponent } from "../sidebar"
@@ -5,10 +8,12 @@ import { SidebarProvider } from "../ui/sidebar"
 
 export const PageLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <SidebarProvider>
-            <Header />
-            <SidebarComponent/>
-            <main className="min-h-[1500px] max-w-[120rem]">
+        <SidebarProvider defaultOpen={false}>
+            <CartProvider>
+                <Header />
+                <SidebarComponent/>
+            </CartProvider>
+            <main className="my-12">
                 {children}
             </main>
             <Footer />
