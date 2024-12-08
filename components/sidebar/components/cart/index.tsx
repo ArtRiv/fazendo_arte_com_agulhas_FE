dbf8a75eb5b8e26df70063046b543477e14d46f0
ArtRiv <arthurfelaco707@gmail.com>
2024-12-08 00:15:25 -0300

    feat: add sidebar cart list

---
+++ components/sidebar/components/cart/index.tsx
@@ -0,0 +1,25 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { SidebarMenu } from "@/components/ui/sidebar"
+import { useCart } from "@/hooks/use-cart";
+import { CartItem } from "./cart-item";
+import { CartItemType } from "@/types/cart/cart-item";
+
+export const CartList = () => {
+    const { items, addOneToCart, removeOneFromCart, deleteFromCart, getProductQuantity } = useCart();
+    
+    return (
+        <ScrollArea className="h-96 w-full rounded-md border pr-4">
+            <SidebarMenu className="flex gap-3">
+                {items.map((item) => (
+                    <CartItem
+                        item={item}
+                        addOneToCart={addOneToCart}
+                        removeOneFromCart={removeOneFromCart}
+                        deleteFromCart={deleteFromCart}
+                        getProductQuantity={getProductQuantity}
+                    />
+                ))}
+            </SidebarMenu>
+        </ScrollArea>
+    )
+}
