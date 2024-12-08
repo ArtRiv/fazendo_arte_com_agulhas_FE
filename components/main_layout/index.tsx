34844880a2c4cfdcbc3b833ee177e9a08831e3a8
ArtRiv <arthurfelaco707@gmail.com>
2024-12-08 00:12:46 -0300

    feat: add sidebar provider to main layout

--- components/main_layout/index.tsx
+++ components/main_layout/index.tsx
@@ -1,14 +1,19 @@
 import { Footer } from "../footer"
 import { Header } from "../header"
+import { SidebarComponent } from "../sidebar"
+import { SidebarProvider } from "../ui/sidebar"
 
 export const PageLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
-            <Header />
-            <main className="my-12">
-                {children}
-            </main>
-            <Footer />
+            <SidebarProvider defaultOpen={false}>
+                <Header />
+                <main className="my-12">
+                    {children}
+                </main>
+                <Footer />
+                <SidebarComponent/>
+            </SidebarProvider>
         </>
     )
 } 
