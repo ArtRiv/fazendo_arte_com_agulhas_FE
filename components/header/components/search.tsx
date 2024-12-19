f52cf9b7518b04699df5add3e6a0d9a19027f447
ArtRiv <arthurfelaco707@gmail.com>
2024-12-18 23:33:01 -0300

    chore: remove extra whitespace in classname

--- components/header/components/search.tsx
+++ components/header/components/search.tsx
@@ -100,10 +100,7 @@ export const SearchInput = () => {
                 {isOpen && searchTerm && (
                     <div
                         data-state={isOpen ? "open" : "closed"}
-                        className={`absolute top-full z-50 min-w-[8rem] max-w-full overflow-hidden rounded-md border bg-popover p-2 text-popover-foreground shadow-md 
-                                data-[state=open]:animate-in data-[state=closed]:animate-out 
-                                data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 
-                                data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95`}
+                        className={`absolute top-full z-50 min-w-full max-w-full overflow-hidden rounded-md border bg-popover p-2 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95`}
                     >
                         <header className="m-2 flex items-center justify-between">
                             <h4 className="font-light text-sm leading-none">
